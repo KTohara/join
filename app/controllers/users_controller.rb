ca8e61d7119e07bf80aae9447041f940dea04aed
kenta <ktohara@gmail.com>
2023-01-08 21:23:31 -0500

    update n+1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = Post.where(user_id: @user).order(created_at: :desc)
+    @posts = Post.includes([:user, :author]).where(user_id: @user).order(created_at: :desc)
     @post = @user.posts.build
   end
 
