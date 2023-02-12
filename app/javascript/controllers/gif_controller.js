ce99199925ac7f11c6b43836b4e896b76f702e12
kenta <ktohara@gmail.com>
2023-02-11 21:56:20 -0500

    wip gif stimulus controller

--- app/javascript/controllers/gif_controller.js
+++ app/javascript/controllers/gif_controller.js
@@ -2,8 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="gif"
 export default class extends Controller {
-  static targets = ['field', 'image', 'preview', 'gifMenu']
-  connect() {
+  static targets = ['field', 'image', 'preview', 'popup', 'button']
+
+  // connect() {
+  // }
+
+  setGifToggler() {
+    // debugger
+    // this.closeGifs = this.closeGifs.bind(this);
+    // const button = document.getElementById('gif_button');
+    // button.setAttribute('aria-expanded', 'true');
+    // window.addEventListener('click', this.closeGifs);
   }
 
   insertGif(event) {
@@ -13,11 +22,25 @@ export default class extends Controller {
       this.fieldTarget.value = url
       this.previewTarget.src = url
       this.previewTarget.classList.remove('hidden')
-      this.close();
+      this.closeGifs();
     }
   }
 
-  close() {
-    this.gifMenuTarget.classList.add('hidden')
+  closeGifs(event) {
+    // debugger
+    // const gifs = this.popupTarget;
+    // const button = document.getElementById('gif_button');
+
+    // if (button.contains(event.target) && button.getAttribute('aria-expanded') === 'true') {
+    //   event.preventDefault();
+    // }
+
+    // if (event && gifs.contains(event.target)) return;
+
+    // gifs.classList.remove('animate-slideDown');
+    gifs.classList.add('hidden');
+
+    // button.setAttribute('aria-expanded', 'false');
+    // window.removeEventListener('click', this.closeGifs);
   }
 }
